f92c9ec8c3ea8cd03b3c333f17be1b0afe2e5ba9
hasan <hasanocal7@gmail.com>
2023-09-20 00:24:45 +0300

    Added Modules and Database

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 // Modules
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Blog = require('./models/Blog');
 
+// Create a App
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.render('index', {blogs});
 });
 
 app.get('/about', (req, res) => {
@@ -25,6 +37,12 @@ app.get('/add_post', (req, res) => {
 
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+// Action
+app.post('/blogs', async (req, res) => {
+  await Blog.create(req.body);
+  res.redirect('/');
 });
 
 // Port
